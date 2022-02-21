49b75ab8159e23ab3a4f5d6366d118242b74cc4d
BuildTools <unconfigured@null.spigotmc.org>
2022-02-21 13:19:15 +0100

    Added generateMessage function, updated Message class

--- Factor_Graph.py
+++ Factor_Graph.py
@@ -1,8 +1,10 @@
+import math
+
 class Message():
-	def __init__(self, node, edge):
-		self.node = node
-		self.edge = edge
-	
+	def __init__(self, i, o):
+		self.i = i
+		self.o = o
+		
 
 
 class Node():
@@ -17,6 +19,10 @@ class Node():
 	
 	def addEdge(self, edge):
 		self.edges.append(edge)
+		
+	def printEdges(self):
+			for obj in self.edges:
+				print(obj.varName)
 		
 	def createFunction():
 		pass
@@ -35,7 +41,6 @@ class Edge():
 	def addNode(self, node):
 		self.nodes.append(node)
 		node.addEdge(self)
-		print(self.nodes[-1].name)
 	
 	def printNodes(self):
 		for obj in self.nodes:
@@ -61,16 +66,47 @@ def createNodes(nNodes, nodeName="f", startVal=1):
 		nodes = Node(-1, nodeName)
 	return nodes
 
-print("Hello world")
+def generateMessage(sender, recip):
+	if (isinstance(sender, Edge)):
+		if( len(sender.nodes) > 1 ): # Not a half-edge -> recursivity needed
+			print("This is not a half-edge")
+			for obj in sender.nodes: 
+				if (obj != recip): #Only enter other connected nodes
+					#1. Check if message is already generated
+					#2. If not -> generate message for this node
+					pass
+		elif(len(sender.nodes) == 1): # Half-edge -> no recursivity needed
+			#1 Generate Message, should be 1 for half edges
+			print("This is a half edge")
+				
+		else: #Something went wrong, no message can be generated
+			print("Error: Unconnected edge")
+		return
+	elif (isinstance(sender, Node)):
+		if ( len(sender.edges) > 1): # Not a leaf node -> recursivity needed
+			print("This is not a leaf node")
+		elif (len(sender.edges) == 1):
+			print("This is a leaf node") #Leaf node -> no recursivity needed
+		else:
+			print("ERROR: Unconnected node")
+		return
+	else:
+		print("ERROR: Incorrect input")
+
 E = createEdges(1, 2, "E")
 B = createEdges(1, 2, "B")
 C = createEdges(4, 2, "C")
 fB = createNodes(1, "fB")
 fE = createNodes(1, "fE")
 g = createNodes(1, "g")
+T = createEdges(1, 2, "C")
+
 E.addNode(fE)
 B.addNode(fB)
 E.addNode(g)
 B.addNode(g)
-
-
+T.addNode(fE)
+generateMessage(E, fE)
+generateMessage(E, fB)
+generateMessage(T, fE)
+generateMessage(C[0], fB)
